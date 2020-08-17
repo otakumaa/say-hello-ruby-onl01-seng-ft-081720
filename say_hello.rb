be6304a2a54a27fa847a844d2043fe1bfd7b8df1
Bryce Ancheta <bryceancheta@gmail.com>
2020-08-17 21:49:23 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,3 +1,7 @@
-def say_hello (name="Ruby Programming")
-  puts "Hello #{name}!"
+def say_hello(name)
+puts "Hello Kent Beck"
 end
+
+def say_hello(name="Ruby Programmer!")
+  puts ("Hello #{name}!")
+end 
